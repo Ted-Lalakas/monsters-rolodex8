76d2e10e0ad07cc0063b9083bc8124a0863e6d74
Ted Lalakas (DJCS) <Ted.Lalakas@justice.vic.gov.au>
2019-12-03 15:39:08 +1100

    Updates

---
+++ precache-manifest.1bf645c1f93c26d581b9954a5a2aa5fc.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d340a67935fa98c5db8c6c89b2dea135",
+    "url": "/monsters-rolodex8/index.html"
+  },
+  {
+    "revision": "894ccbe040c4d45f1a2c",
+    "url": "/monsters-rolodex8/static/css/main.bb8376fe.chunk.css"
+  },
+  {
+    "revision": "afaa14f5cd0f1f3476ff",
+    "url": "/monsters-rolodex8/static/js/2.31bdc674.chunk.js"
+  },
+  {
+    "revision": "894ccbe040c4d45f1a2c",
+    "url": "/monsters-rolodex8/static/js/main.16ab375f.chunk.js"
+  },
+  {
+    "revision": "412d695147de623b5e51",
+    "url": "/monsters-rolodex8/static/js/runtime-main.70c5e1f2.js"
+  }
+]);
